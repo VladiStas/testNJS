d9fbf49ad5ffe09cc4f36ef911a27e9bd2107204
Vlad <91955559+VladiStas@users.noreply.github.com>
2024-09-13 23:52:19 +0300

    Debug = True :+1:

--- yatest/yatest/settings.py
+++ yatest/yatest/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z&b9s^&mc-do9q19f#6bou!$)1230fgstgis&#f@e9o08c)c+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
